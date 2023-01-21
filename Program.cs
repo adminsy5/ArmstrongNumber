1a495236a5ed0b327638eca0d9496b99daafdcf1
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-21 14:52:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Armstrog number in Vb.net
+namespace ArmstrongNumber
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int num, rem, temp, sum = 0;
+            Console.WriteLine("Enter the number : ");
+            num=Convert.ToInt32(Console.ReadLine());
+            temp = num;
+
+            while(num>0)
+            {
+                rem = num % 10;
+                sum = sum + rem * rem * rem;
+                num = num / 10;
+            }
+            if(temp==sum)
+            {
+                Console.WriteLine("This is Armstrong Number !");
+            }
+            else
+            {
+                Console.WriteLine("This is nt Armstrong Number !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
